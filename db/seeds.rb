9886b9d2b95e9fa07654440bd9ef4a7de67f6f03
jgdimeglio <jgdimeglio@gmail.com>
2013-11-17 03:59:36 -0300

    Arreglo del layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,26 @@
-user = User.create(:email => 'admin@test.com',
+ User.create(:email => 'admin@test.com',
 					:name => 'admin', 
 					:admin => true,
 				        :password => "Admin1234")
+
+
+
+ Advert.create(:title => 'Publicity',
+					:text => 'Compre compre!', 
+					:enable => 'No')
+
+ Advert.create(:title => 'Publicom',
+					:text => 'Ofertas !', 
+					:enable => 'Yes')
+
+ Advert.create(:title => 'Publicate.com',
+					:text => 'Ultimas ofertas!', 
+					:enable => 'Yes')
+
+ Advert.create(:title => 'Publicaya.com.ar',
+					:text => 'No te pierdas esta oportunidad', 
+					:enable => 'Yes')
+
+ Advert.create(:title => 'republica.com',
+					:text => 'ultimas novedades', 
+					:enable => 'No')
